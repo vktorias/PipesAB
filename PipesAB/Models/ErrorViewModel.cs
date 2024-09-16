4f5cb4f4d3e0bfadec4c3d70a3748ab0fca6ae76
viktoria wallström <viktoria-wallstrom@hotmail.com>
2024-09-16 10:05:38 +0200

    Add project files.

---
+++ PipesAB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PipesAB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
